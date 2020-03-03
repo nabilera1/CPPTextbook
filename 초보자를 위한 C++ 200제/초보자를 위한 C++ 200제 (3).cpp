fbbe38ff13952752e927470beda9bb28db7891da
KIMDONGGYUN <nabilera1@naver.com>
2020-03-04 00:28:21 +0900

    0303

--- 초보자를 위한 C++ 200제/초보자를 위한 C++ 200제 (3).cpp
+++ 초보자를 위한 C++ 200제/초보자를 위한 C++ 200제 (3).cpp
@@ -432,46 +432,418 @@ int main()
 
 
 //1을 100억 번 더하는 시간 : 25초
+//컴퓨터 마다 처리 시간은 다르다.
+
+
+
+
+
+
+
+000
+학습내용 :   이해하기;
+
+
+
+
+
+
+
+
+
+053
+학습내용 : string.compare;
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string seven_war = "임진왜란";
+	string korea_war = "한국전쟁";
+
+	if (seven_war.compare(korea_war) == 0)//값이 같으면 0
+		cout << "같은 문자열입니다\n";
+	else
+		cout << "다른 문자열입니다\n";
+
+	return 0;
+};
+
+
+//다른 문자열입니다
+
+
+
+
+
+
+
+054
+학습내용 : 문자열 조회하기;
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string baekhap_war = "백합벌 전투 : 백제 왕자 부여창의 일기토로 유명한 전투";
+
+	int rtn = baekhap_war.find("부여창");
+
+	if (rtn > 0)//찾지 못하면 -1 리턴
+		cout << "문자열을 찾았습니다. 위치는 " << rtn << "입니다." << endl;
+	else
+		cout << "문자열을 찾을 수 없습니다." << endl;
+
+	return 0;
+};
+
+//문자열을 찾았습니다. 위치는 24입니다.
+//한글은 2칸, 스페이스는 1칸, 콜론도 1칸 총 24번째 시작 위치를 가리킴
+
+
+
+
+055
+학습내용 : 문자열 길이 구하기;
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string jinduk_name = "김승만";
+	string jinsung_name = "kim man";
+
+	cout << "진덕여왕 이름 길이 : " << jinduk_name.length() << endl;
+	cout << "진성여왕 이름 길이 : " << jinsung_name.length() << endl;
+
+
+	return 0;
+};
+
+
+//진덕여왕 이름 길이 : 6
+//진성여왕 이름 길이 : 7
+
+
+
+056
+학습내용 : 대소문자 변환;
+
+#include <iostream>
+#include <string>
+#include <locale>
+#include <algorithm>  //transform
+
+using namespace std;
+
+int main()
+{
+	string silla = "divided into the Three Kingdoms.";
+	string joseon = "Yi Seong-gye, established Joseon in 1392.";
+	//문자열을 대소문자로 변환
+	transform(silla.begin(), silla.end(), silla.begin(), toupper);
+	transform(joseon.begin(), joseon.end(), joseon.begin(), tolower);
+
+	char lower_ch = 'g';
+	char upper_ch = 'B';
+	//문자를 대소문자로 변환
+	lower_ch = toupper(lower_ch);
+	upper_ch = tolower(upper_ch);
+
+	cout << "문자열 대문자로 변환 : " << silla << endl;
+	cout << "문자열 소문자로 변환 : " << joseon << endl;
+	cout << "문자 대문자로 변환 : " << lower_ch << endl;
+	cout << "문자 소문자로 변환 : " << upper_ch << endl;
+
+	return 0;
+};
+
+
+//문자열 대문자로 변환 : DIVIDED INTO THE THREE KINGDOMS.
+//문자열 소문자로 변환 : yi seong - gye, established joseon in 1392.
+//문자 대문자로 변환 : G
+//문자 소문자로 변환 : b
+
+
+
+
+057
+학습내용 : 문자열 합치기와 추가하기;
+//append 함수와 += 연산자는 같은 역할을 한다.
+//string은 내부버퍼를 가지고 있어 가변 길이 문자열 처리가 가능
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string king = "조선 세종";
+	string favorite1 = "고기";
+	string favorite2 = "야근";
+
+	string king_info = "";
+
+	king_info += king;
+	king_info += "은 ";
+	king_info += favorite1;
+	king_info.append("와 ");
+	king_info.append(favorite2);
+	king_info.append("을 좋아했습니다.");
+
+	cout << king_info << endl;
+
+	return 0;
+};
+
+
+//문자열 연결을 위해서는 string의 + 연산자, += 사용
+//append, insert, push_back 함수로도 문자와 문자열 추가 가능
+//string.append("ABC",2)처럼 추가할 문자열의 길이 지정 가능
+//string.append("ABC",1,2)처럼 문자열 일부만 추출해 추가 가능
+//string.insert(2,"ABC",2)
+//string.insert(0,"ABC",1,2)
 //
 
 
 
 
 
+058
+학습내용 : 문자열 추가하기, insert;
 
+#include <iostream>
+#include <string>
 
-000
-학습내용 :   이해하기;
+using namespace std;
 
+int main()
+{
+	string sentence = "i coding";
+	sentence.insert(2, "hate ");//0,1,2번째 자리는 'c' 위치
+	cout << sentence << endl;
 
+	sentence.insert(7, "or like ");
+	cout << sentence << endl;
 
+	return 0;
+};
 
-
-
-
-
-
-000
-학습내용 :   이해하기;
-
-
-
-
-
-
-
-
-
-000
-학습내용 :   이해하기;
+//i hate coding
+//i hate or like coding
 
 
 
 
 
+059
+학습내용 : 문자열 지우기;
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string sentence = "i hate coding";
+
+	sentence.erase(0, 7);//인덱스 0~6번까지 삭제, 즉 7개 삭제
+
+	cout << "i like " << sentence << endl;
+
+	return 0;
+};
+
+//i like coding
+
+
+
+060
+학습내용 : 문자열 이동하기;
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	string str1 = "i like coding";
+	string str2 = move(str1);
+
+	cout << "str1 : " << str1 << endl;//이동되어 빈 문자열
+	cout << "str2 : " << str2 << endl;//str1이 이동
+
+	vector<int> v1 = { 1, 2, 3 };
+	vector<int> v2 = move(v1);
+
+	cout << "v1 size : " << v1.size() << endl;
+	cout << "v2 size : " << v2.size() << endl;
+
+	return 0;
+}
+
+
+
+//str1:
+//str2: i like coding
+//v1 size : 0
+//v2 size : 3
 
 
 
 
+
+061
+학습내용 : remove;
+
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	string sentence = "i like coding";
+
+	sentence.erase(remove(sentence.begin(), sentence.end(), ' '), sentence.end());
+	//ilikecoding, ilikecodingng에서 ng를 제거(erase)
+
+	//remove(sentence.begin(), sentence.end(), ' ');//ilikecodingng 를 sentense 변수에 저장
+
+	cout << sentence << endl;
+	//cout << (int)remove(sentence.begin(), sentence.end(), ' ') << endl;
+
+	return 0;
+};
+
+
+
+062
+학습내용 : replace;
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string sentence = "i like coding";
+	string find_str = "coding";
+	string replace_str = "history";
+
+	sentence.replace(sentence.find(find_str), find_str.length(), replace_str);
+
+	cout << sentence << endl;
+
+	return 0;
+};
+
+//i like history
+
+
+
+
+063
+학습내용 : stoi;
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string str1 = "10";
+	string str2 = "2.456";
+	string str3 = "456 문자열";
+
+	int num1 = stoi(str1);//문자열을 정수로 변환, c++11 이상에서 지원, c언어 atoi와 같음
+	int num2 = stoi(str2);//visual studio 2012 이상시 사용 가능
+	int num3 = stoi(str3);
+
+	cout << num1 << ", " << num2 << ", " << num3 << endl;
+
+	return 0;
+};
+
+
+//10, 2, 456
+
+
+
+
+064
+학습내용 : stringstream;
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+	stringstream ss;
+
+	double number1 = 0.0;
+
+	ss << "1.2,2.6-3.8!4.7=8.9";
+	//변수에 실수와 특수문자로 이루어진 문자열 추가
+	cout << "== string to double ==" << endl;
+	while (!ss.eof())
+	{
+		ss >> number1;//숫자 부분을 읽어 실수형 변수에 저장
+		ss.ignore();//ss가 처음 데이터를 읽고 다시 처음으로 돌아가는 것을 방지
+		//ignore가 없으면 무한루프에 빠짐
+		cout << number1 << ", ";
+	}
+
+	ss.clear();//ss 파일의 끝까지 읽은 부분을 다시 맨 앞부터 읽을 수 있는 상태로 만듬
+
+	ss.str("");
+	/*
+	cout<<endl;
+	string str1;
+	ss >> str1;
+	cout << "str1 " << str1 << endl;
+	ss.clear();
+	*/
+	ss << "1," << "2" << 3 << " " << 4;
+
+	int number2 = 0;
+
+	cout << endl << "== string to int ==" << endl;
+	while (!ss.eof())
+	{
+		ss >> number2;
+		ss.ignore();
+
+		cout << number2 << ", ";
+	}
+
+
+	return 0;
+}
+
+
+//== string to double ==
+//1.2, 2.6, 3.8, 4.7, 8.9,
+//== string to int ==
+//1, 23, 4,
 
 
